a5a3b0e1d29973305f014f8f6cca4775d5b36913
Huongto1505it <huongphuongvn@gmail.com>
2020-10-29 22:44:35 +0700

    them bai 5

---
+++ Baith3/Bai5-th4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baith3
+{
+    //class Doibong
+    //{
+    //    private string tendb, huanlv;
+    //    private int[] cauthu;
+    //    public void Nhap()
+    //    {
+    //        Console.Write("Nhap ten doi bong:"); tendb = Console.ReadLine();
+    //        Console.Write("Nhap ten huan luyen vien:"); huanlv = Console.ReadLine();
+    //        cauthu = new int[11];
+    //        for(int i = 0; i < 11; i++)
+    //        {
+    //            Console.Write("Nhap ten cau thu thu {0}:",i+1);
+    //            cauthu[i] = int.Parse(Console.ReadLine());
+    //        }
+    //    }
+    //}
+    //class Bang
+    //{
+    //    private Doibong[] db;
+    //    public void Nhap()
+    //    {
+    //        db = new Doibong[32];
+    //        Console.WriteLine("Nhap danh sach bang A");
+    //        for (int i = 0; i < 4; i++)
+    //        {
+    //            db[i] = new Doibong();
+    //            db[i].Nhap();
+    //        }
+
+
+
+
+    //    }
+
+    //}
+    //class App
+    //{
+    //    static void Main()
+    //    {
+    //        Bang b = new Bang();
+    //        b.Nhap();
+    //        Console.ReadKey();
+    //    }
+    //}
+}
--- Baith3/Program.cs
+++ Baith3/Program.cs
@@ -6,48 +6,147 @@ using System.Threading.Tasks;
 
 namespace Baith3_4
 {
-    class Doibong
+    class Nguoi
     {
-        private string tendb, huanlv;
-        private int[] cauthu;
-        public void Nhap()
+        private string hoten;
+        private int namsinh;
+        private double chieucao,cannang;
+        public Nguoi()
         {
-            Console.Write("Nhap ten doi bong:"); tendb = Console.ReadLine();
-            Console.Write("Nhap ten huan luyen vien:"); huanlv = Console.ReadLine();
-            cauthu = new int[11];
-            for(int i = 0; i < 11; i++)
+            hoten = " ";
+            namsinh =  0;
+            chieucao = 0;
+            cannang = 0;
+        }
+        public Nguoi(string hoten,int namsinh,double chieucao,double cannang)
+        {
+            this.hoten = hoten;
+            this.namsinh = namsinh;
+            this.cannang = cannang;
+            this.chieucao = chieucao;
+        }
+        public Nguoi(Nguoi t2)
+        {
+            this.hoten = t2.hoten;
+            this.chieucao = t2.chieucao;
+            this.cannang = t2.cannang;
+            this.namsinh = t2.namsinh;
+        }
+        
+        public int Trave()
+        {
+            double k = cannang / (chieucao * chieucao); ;
+            if (k > 25) return 1;
+            else if (k < 19) return -1;
+            else return 0;
+         }
+        public override string ToString()
+
+        {
+            return this.hoten + " " +this.namsinh +this. chieucao + this.cannang;
+        }
+        public string Hoten
+        {
+            get
             {
-                Console.Write("Nhap ten cau thu thu {0}:",i+1);
-                cauthu[i] = int.Parse(Console.ReadLine());
+                int n = hoten.LastIndexOf(" ");
+                return hoten.Substring(n + 1) + " " +hoten.Substring(0, n);
             }
         }
+        public double Chieucao
+        {
+            get
+            {
+                return chieucao;
+
+            }
+            set
+            {
+                if (value > 1.8 || value < 1.2) chieucao = value;
+            }
+        }
+        public double Cannang
+        {
+            get
+            {
+                return cannang;
+            }
+            set
+            {
+                if (value > 35 && value < 150) value = cannang;
+            }
+        }
+        public void Nhap()
+        {
+            Console.Write("Nhap ho ten:");hoten = Console.ReadLine();
+            Console.Write("Nhap nam sinh:"); namsinh = int.Parse(Console.ReadLine());
+            Console.Write("Nhap chieu cao:");chieucao = double.Parse(Console.ReadLine());
+            Console.Write("Nhap can nang:"); cannang = double.Parse(Console.ReadLine());
+        }
+        
+        public Nguoi Cong(Nguoi t2)
+        {
+            Nguoi t = new Nguoi();
+            t.cannang = this.cannang + t2.cannang;
+            return t;
+        }
+        public Nguoi Tong(double f)
+        {
+            Nguoi t = new Nguoi();
+            t.cannang = this.cannang + f;
+            return t;
+        } 
     }
-    class Bang
+    class QlP
     {
-        private Doibong[] db;
+        private Nguoi[] ds;
         public void Nhap()
         {
-            db = new Doibong[32];
-            Console.WriteLine("Nhap danh sach bang A");
-            for (int i = 0; i < 4; i++)
+
+            ds = new Nguoi[5];
+            for (int i = 0; i < 5; i++)
             {
-                db[i] = new Doibong();
-                db[i].Nhap();
+                Console.WriteLine("Nhap nguoi thu {0}", i + 1);
+                ds[i] = new Nguoi();
+                ds[i].Nhap();
             }
-
-            
-
-
         }
+        public void Hien()
+        {
+            for (int i = 0; i < 5; i++)
+                ds[i].ToString();
+        }
+        public void Hiends()
+        {
+            for (int i = 0; i < 5; i++)
+                if (ds[i].Chieucao > 1.7 && ds[i].Cannang > 70)
+                    ds[i].ToString();
+        }
+        public void Hiensk()
+        {
+            for (int i = 0; i < 5; i++)
+                do
+                {
+                    if (ds[i].Trave() == -1)
+                    {
+                        ds[i].Cannang++;
 
+                        ds[i].ToString();
+                    }
+                } while (ds[i].Trave() != 1);
+        }
     }
     class App
     {
         static void Main()
         {
-            Bang b = new Bang();
-            b.Nhap();
+            QlP t = new QlP();
+            t.Nhap();
+            t.Hien();
+            t.Hiends();
+            t.Hiensk();
             Console.ReadKey();
+
         }
     }
 }
